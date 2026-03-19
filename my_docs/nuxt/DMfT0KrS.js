fc5c6b4e6af564d5a9826b801152838186807c0e
Celia Charlotte <celiajiang98@gmail.com>
2026-03-19 15:07:45 +0000

    v4.0

---
+++ my_docs/nuxt/DMfT0KrS.js
@@ -0,0 +1,1 @@
+import{r as a}from"./DiN5jumI.js";import t from"./Dx2fv1tP.js";import{_ as s,a as e,p as n,s as p,o as c}from"./BTIug4Ia.js";import"./BN_7HF1G.js";const d={__name:"ProseCaution",setup(i){const o=s();return(r,f)=>(c(),e(t,{color:"error",icon:p(o).ui.icons.caution},{default:n(()=>[a(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
